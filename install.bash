16b921ab9a589571a4befbc2ecfde0ef4616f5eb
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-05 11:46:05 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -19,37 +19,15 @@ fi
 (cd $origin && tar --exclude-vcs --create --file - .) | tar --extract --verbose --file -
 composer config bin-dir bin
 # cp $origin/.env.dist . # Needs apparently to be done before install.
+
 /usr/bin/time composer install
 /usr/bin/time composer require annotations # sensio/framework-extra-bundle
-# composer remove --dev symfony/profiler-pack
-#^ Dependency "symfony/twig-bundle" is also a root requirement, but is not explicitly whitelisted. Ignoring.
-# install --directory config # Is it really needed?
 # composer require symfony/yaml # in symfony/skeleton
 # composer require symfony/console # in symfony/skeleton
 /usr/bin/time composer require twig # symfony/twig-bundle
 /usr/bin/time composer require server # symfony/web-server-bundle
-## composer require sensio/framework-extra-bundle # in symfony/website-skeleton
-# composer require symfony/orm-pack # in symfony/website-skeleton
-# composer require symfony/swiftmailer-bundle # in symfony/website-skeleton
-# composer require symfony/security-csrf
-# cp $origin/config/packages/*.yaml config/packages --verbose
-# cp $origin/config/routes/*.yaml config/routes --verbose
-# composer require friendsofsymfony/user-bundle
-/usr/bin/time composer require css-selector # symfony/css-selector
+/usr/bin/time composer require css-selector # symfony/css-selector # Needed by a test!
 /usr/bin/time composer require browser-kit # symfony/browser-kit
 /usr/bin/time composer require simple-phpunit # symfony/phpunit-bridge
-
-# cp $origin/src/Entity/*.php src/Entity --verbose # May be done earlier.
-# bin/console doctrine:database:create
-# bin/console doctrine:migrations:diff --quiet
-# bin/console doctrine:migrations:migrate --no-interaction --quiet
-# bin/console doctrine:schema:update --force
-# composer require doctrine/doctrine-fixtures-bundle --dev
-# cp $origin/src/DataFixtures/AppFixtures.php src/DataFixtures
-# bin/console doctrine:fixtures:load --append
-# bin/console fos:user:create superadmin superadmin@example.com superadmin --super-admin
-# bin/console fos:user:create user user@example.com user
-
-# cp --recursive $origin/tests . --verbose
 
 bin/console assets:install --symlink
